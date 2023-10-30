d40f3aa4aff0ee48ba1c845b9cdb04f28387164e
allen404 <20906721+allen404@users.noreply.github.com>
2023-10-30 20:17:55 +0700

    change numbering, upload db file

---
+++ database.sql
@@ -0,0 +1,281 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : pgsql_localhost
+ Source Server Type    : PostgreSQL
+ Source Server Version : 150000 (150000)
+ Source Host           : localhost:5432
+ Source Catalog        : ozora_tech_test
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 150000 (150000)
+ File Encoding         : 65001
+
+ Date: 30/10/2023 20:13:48
+*/
+
+
+-- ----------------------------
+-- Sequence structure for barang_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."barang_id_seq";
+CREATE SEQUENCE "public"."barang_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for failed_jobs_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."failed_jobs_id_seq";
+CREATE SEQUENCE "public"."failed_jobs_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for migrations_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."migrations_id_seq";
+CREATE SEQUENCE "public"."migrations_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for personal_access_tokens_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."personal_access_tokens_id_seq";
+CREATE SEQUENCE "public"."personal_access_tokens_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Sequence structure for users_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."users_id_seq";
+CREATE SEQUENCE "public"."users_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for barang
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."barang";
+CREATE TABLE "public"."barang" (
+  "id" int8 NOT NULL DEFAULT nextval('barang_id_seq'::regclass),
+  "nama_barang" text COLLATE "pg_catalog"."default" NOT NULL,
+  "harga_beli" int8 NOT NULL,
+  "harga_jual" int8 NOT NULL,
+  "stok" int4 NOT NULL,
+  "foto_barang" text COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of barang
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for failed_jobs
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."failed_jobs";
+CREATE TABLE "public"."failed_jobs" (
+  "id" int8 NOT NULL DEFAULT nextval('failed_jobs_id_seq'::regclass),
+  "uuid" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "connection" text COLLATE "pg_catalog"."default" NOT NULL,
+  "queue" text COLLATE "pg_catalog"."default" NOT NULL,
+  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+  "exception" text COLLATE "pg_catalog"."default" NOT NULL,
+  "failed_at" timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+;
+
+-- ----------------------------
+-- Records of failed_jobs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."migrations";
+CREATE TABLE "public"."migrations" (
+  "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq'::regclass),
+  "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "batch" int4 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of migrations
+-- ----------------------------
+INSERT INTO "public"."migrations" VALUES (1, '2014_10_12_000000_create_users_table', 1);
+INSERT INTO "public"."migrations" VALUES (2, '2014_10_12_100000_create_password_reset_tokens_table', 1);
+INSERT INTO "public"."migrations" VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
+INSERT INTO "public"."migrations" VALUES (4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+INSERT INTO "public"."migrations" VALUES (5, '2023_10_30_082224_create_barang_table', 1);
+
+-- ----------------------------
+-- Table structure for password_reset_tokens
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."password_reset_tokens";
+CREATE TABLE "public"."password_reset_tokens" (
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "created_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of password_reset_tokens
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for personal_access_tokens
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."personal_access_tokens";
+CREATE TABLE "public"."personal_access_tokens" (
+  "id" int8 NOT NULL DEFAULT nextval('personal_access_tokens_id_seq'::regclass),
+  "tokenable_type" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "tokenable_id" int8 NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "token" varchar(64) COLLATE "pg_catalog"."default" NOT NULL,
+  "abilities" text COLLATE "pg_catalog"."default",
+  "last_used_at" timestamp(0),
+  "expires_at" timestamp(0),
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of personal_access_tokens
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."users";
+CREATE TABLE "public"."users" (
+  "id" int8 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "email_verified_at" timestamp(0),
+  "password" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "remember_token" varchar(100) COLLATE "pg_catalog"."default",
+  "created_at" timestamp(0),
+  "updated_at" timestamp(0)
+)
+;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."barang_id_seq"
+OWNED BY "public"."barang"."id";
+SELECT setval('"public"."barang_id_seq"', 16, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."failed_jobs_id_seq"
+OWNED BY "public"."failed_jobs"."id";
+SELECT setval('"public"."failed_jobs_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."migrations_id_seq"
+OWNED BY "public"."migrations"."id";
+SELECT setval('"public"."migrations_id_seq"', 5, true);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."personal_access_tokens_id_seq"
+OWNED BY "public"."personal_access_tokens"."id";
+SELECT setval('"public"."personal_access_tokens_id_seq"', 1, false);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."users_id_seq"
+OWNED BY "public"."users"."id";
+SELECT setval('"public"."users_id_seq"', 1, false);
+
+-- ----------------------------
+-- Uniques structure for table barang
+-- ----------------------------
+ALTER TABLE "public"."barang" ADD CONSTRAINT "barang_nama_barang_unique" UNIQUE ("nama_barang");
+
+-- ----------------------------
+-- Primary Key structure for table barang
+-- ----------------------------
+ALTER TABLE "public"."barang" ADD CONSTRAINT "barang_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_uuid_unique" UNIQUE ("uuid");
+
+-- ----------------------------
+-- Primary Key structure for table failed_jobs
+-- ----------------------------
+ALTER TABLE "public"."failed_jobs" ADD CONSTRAINT "failed_jobs_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table migrations
+-- ----------------------------
+ALTER TABLE "public"."migrations" ADD CONSTRAINT "migrations_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table password_reset_tokens
+-- ----------------------------
+ALTER TABLE "public"."password_reset_tokens" ADD CONSTRAINT "password_reset_tokens_pkey" PRIMARY KEY ("email");
+
+-- ----------------------------
+-- Indexes structure for table personal_access_tokens
+-- ----------------------------
+CREATE INDEX "personal_access_tokens_tokenable_type_tokenable_id_index" ON "public"."personal_access_tokens" USING btree (
+  "tokenable_type" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST,
+  "tokenable_id" "pg_catalog"."int8_ops" ASC NULLS LAST
+);
+
+-- ----------------------------
+-- Uniques structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_token_unique" UNIQUE ("token");
+
+-- ----------------------------
+-- Primary Key structure for table personal_access_tokens
+-- ----------------------------
+ALTER TABLE "public"."personal_access_tokens" ADD CONSTRAINT "personal_access_tokens_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_email_unique" UNIQUE ("email");
+
+-- ----------------------------
+-- Primary Key structure for table users
+-- ----------------------------
+ALTER TABLE "public"."users" ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
