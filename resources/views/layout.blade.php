1dfe4ccf0a0647beaf492a3c7812e42e97ef037c
allen404 <20906721+allen404@users.noreply.github.com>
2023-10-30 19:46:11 +0700

    change to cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,10 @@
 
     <title>Ozora Technical Test</title>
 
-    <link href="{{asset('assets/datatables/datatables.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/bootstrap-5.3.2/css/bootstrap.css')}}" rel="stylesheet">
-    <script src="{{asset('assets/jquery/jquery-3.6.3.js')}}" ></script>
-    <script src="{{asset('assets/datatables/datatables.js')}}"></script>
-    <script src="{{asset('assets/bootstrap-5.3.2/js/bootstrap.bundle.js')}}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
